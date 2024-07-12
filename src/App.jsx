a4d34e6b35a867867ca375e6cadcbf406999254d
amerihn <amerihn.art@gmail.com>
2024-07-12 23:17:00 +0700

    add to cart function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 
 import React, { useState } from 'react';
 import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+// import './App.css';
+// import AboutUs from './AboutUs';
+import CartItem from './CartItem';
 
 function App() {
   
@@ -14,26 +15,14 @@ function App() {
 
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
+      
 
-      </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+
+      <div 
+      // className={`product-list-container ${showProductList ? 'visible' : ''}`}
+      >
         <ProductList />
+        <CartItem />
       </div>
     </div>
   );
@@ -41,5 +30,22 @@ function App() {
 
 export default App;
 
+//       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+//<div className="background-image"></div>
+//<div className="content">
+// <div className="landing_content">
+//  <h1>Welcome To Paradise Nursery</h1>
+//   <div className="divider"></div>
+//   <p>Where Green Meets Serenity</p>
+ 
+//   <button className="get-started-button" onClick={handleGetStartedClick}>
+//     Get Started
+//   </button>
+//  </div>
+//   {/* <div className="aboutus_container">
+//   <AboutUs/>
+//   </div> */}
+//   </div>
+// </div>
 
 
--- src/CreatSlice.jsx
+++ src/CreatSlice.jsx
@@ -3,20 +3,62 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CreatSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    cartItems: [], // Initialize items as an empty array
   },
   reducers: {
-    addItem: (state, action) => {
-    
+    addItemToCart(state, action) {
+      // const { name, image, cost } = action.payload;
+      const existingItem = state.cartItems.find(
+        item => item.index === action.payload
+      );
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.cartItems.push(
+          { ...action.payload, quantity: 1 }
+        );
+      };
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-    
-    },
+    // removeItemFromCart: (state, action) => {
+    //   // state.cartItems = state.cartItems.filter(
+    //   //   item => item.index !== action.payload.name
+    //   // );
+    //   state.cartItems.map((item) => {
+    //     if (item.index === action.payload) {
+    //       state.cartItems = state.cartItems.filter(
+    //         (item) => item.index !== item.index
+    //       );
+    //     }})
+    // },
+    // increaseItemQuantity: (state, action) => {
+    //   const itemToIncrease = state.cartItems.find(
+    //     item => item.index === action.payload.name
+    //   );
+    //   console.log(itemToIncrease);
+    //   // itemToIncrease.forEach((item) => {
+    //   //   if (itemToIncrease) {
+    //   //     item.quantity +=1;
+    //   //   }
+    //   // })
+    //   if (itemToIncrease) {
+    //     itemToIncrease.quantity +=1;
+    //   }
+    // },
+    // decreaseItemQuantity: (state, action) => {
+    //   // const { name, quantity } = action.payload;
+    //   const itemToDecrease = state.cartItems.find(
+    //     item => item.name === action.payload
+    //   );
+    //   if (itemToDecrease && itemToDecrease.quantity > 1) {
+    //     itemToDecrease.quantity--;
+    //   }
+    // },
+
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
+export const { 
+  addItemToCart, removeItemFromCart, increaseItemQuantity, decreaseItemQuantity
+} = CreatSlice.actions;
 
 export default CreatSlice.reducer;
